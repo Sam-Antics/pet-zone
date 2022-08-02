0ab3066314086269660f2c99cb0341a2e6bf75ff
Samantha Guerra <88997930+Sam-Antics@users.noreply.github.com>
2022-08-02 16:35:26 -0500

    added immunization seed file

--- seeds/index.js
+++ seeds/index.js
@@ -1,15 +1,18 @@
 const seedStaff = require('./staff-seeds');
 const seedDepartments = require('./departments-seeds');
+const seedImmunizations = require('./immunizations-seeds');
 
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  console.log('\n-----DATABASE SYNCED----\n-');
+  console.log('\n===== DATABASE SYNCED =====\n-');
   await seedStaff();
-  console.log('\n-----STAFF SEEDED-----\n');
+  console.log('\n===== STAFF SEEDED =====\n');
   await seedDepartments();
-  console.log('\n----- DEPARTMENTS SEEDED -----\n');
+  console.log('\n===== DEPARTMENTS SEEDED =====\n');
+  await seedImmunizations();
+  console.log('\n===== IMMUNIZATIONS SEEDED =====\n')
  // other seed tables here
 
   process.exit(0);

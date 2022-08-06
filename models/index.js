6d17fc771fd40aee0b3fc7d3fde59885e1308dcb
chris <christiancarmona3@gmail.com>
2022-08-06 18:10:15 -0500

    testest routes and some frontend

--- models/index.js
+++ models/index.js
@@ -1,10 +1,10 @@
 // import models
 
 const User = require('./users');
-// const Staff = require('./staff');
+const Staff = require('./staff');
 // const Department = require('./departments');
 // const Owner = require('./owners');
-// const Pet = require('./pets');
+const Pet = require('./pets');
 // const Comment = require('./comments');
 // const Immunization = require('./immunizations');
 // const Boarding = require('./boardings');
@@ -93,7 +93,7 @@ const User = require('./users');
 //   foreignKey: "staff_id",
 // });
 
-module.exports = { User };
+module.exports = { User, Staff, Pet };
 
 
 //Future Options:
--- seeds/index.js
+++ seeds/index.js
@@ -1,19 +1,19 @@
-// const seedStaff = require('./staff-seeds');
+const seedStaff = require('./staff-seeds');
 // const seedDepartments = require('./departments-seeds');
 // const seedImmunizations = require('./immunizations-seeds');
 // const seedGrooming = require('./grooming-seeds');
 // const seedBoarding = require('./boarding-seeds');
 const seedUser = require('./user-seeds');
-// const seedPet = require('./pet-seeds')
+const seedPet = require('./pet-seeds');
 
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
   // clinic-side table seeds
   await sequelize.sync({ force: true });
-  // console.log('\n===== DATABASE SYNCED =====\n-');
-  // await seedStaff();
-  // console.log('\n===== STAFF SEEDED =====\n');
+  console.log('\n===== DATABASE SYNCED =====\n-');
+  await seedStaff();
+  console.log('\n===== STAFF SEEDED =====\n');
   // await seedDepartments();
   // console.log('\n===== DEPARTMENTS SEEDED =====\n');
   // await seedImmunizations();
@@ -24,8 +24,8 @@ const seedAll = async () => {
   // console.log('\n===== BOARDING SEEDED =====\n');
   await seedUser();
   console.log('\n===== User SEEDED =====\n');
-  // await seedPet();
-  // console.log('\n===== Pet SEEDED =====\n');
+  await seedPet();
+  console.log('\n===== Pet SEEDED =====\n');
  // other seed tables here
 
   process.exit(0);

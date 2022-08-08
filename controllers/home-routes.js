8bfb83ef046dc9200d59e57dfecd7189ce5cad0c
moliveros <mcoliveros1202@gmail.com>
2022-08-08 18:48:05 -0500

    updated some landing page code and amended routes files names

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,18 +1,10 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 
+router.get('/', (req, res) => {
+    res.render('landing', {
+        
+    })
+})
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/dashboard')
-        return;
-    }
-    res.render('login');
-});
-
-router.get('/signup', (req, res) => {
-    res.render('signup');
-});
-
-
-module.exports = router
+module.exports = router;

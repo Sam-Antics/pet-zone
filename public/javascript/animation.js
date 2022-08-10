9a66d9e524753714e0e9d9a3f6cb887a389c7a38
chris <christiancarmona3@gmail.com>
2022-08-10 13:49:31 -0500

    small changes

--- public/javascript/animation.js
+++ public/javascript/animation.js
@@ -24,26 +24,3 @@ anime({
   duration: 10000
 })
 
-// anime({
-//   targets: '.form-div',
-//   keyframes: [
-//       {translateX: 1130},
-//       {rotateY: 180},
-//       {translateX: 0},
-//       {rotateY: 0}
-//     ],
-//   easing: 'easeInOutSine',
-//   loop: true,
-//   duration: 8000
-// });
-
-// anime ({
-//   targets: '.form-div',
-//   delay: 3000,
-//   translateY: path('y'),
-//   translateX: path('x'),
-//   rotate: path('angle'),
-//   easing: 'linear',
-//   loop: true,
-//   duration: 5000
-// })

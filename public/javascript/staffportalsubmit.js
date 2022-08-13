1d5bea26b512a678c643af232e7c726acefedcbe
chris <christiancarmona3@gmail.com>
2022-08-13 12:55:28 -0500

    corrected form handler

--- public/javascript/staffportalsubmit.js
+++ public/javascript/staffportalsubmit.js
@@ -1,36 +1,38 @@
 // PET FORMS
 async function addPetFormhandler(event) {
-    event.preventDefault();
-  
-    const petName = document.querySelector('input[name="pet-name"]').value.trim();
-    const petType = document.querySelector('input[name="pet-type"]').value.trim();
-    const petBreed = document.querySelector('input[name="pet-breed"]').value.trim();
-    const petAge = document.querySelector('input[name="pet-age"]').value.trim();
-    const petColor = document.querySelector('input[name="pet-color"]').value.trim();
-    const petOwner = document.querySelector('input[name="pet-owner"]').value.trim();
-  
-    if (petName && petOwner ) {
-      const response = await fetch('/api/pets', {
-        method: 'post',
-        body: JSON.stringify({
-          name,
-          type,
-          breed,
-          age,    
-          color,
-          owner_id
-        }),
-        headers: { 'Content-Type': 'application/json' },
-      });
+ 
+  event.preventDefault();
+  const name = document.querySelector('input[name="pet-name"]').value.trim();
+  const type = document.querySelector('input[name="pet-type"]').value.trim();
+  const breed = document.querySelector('input[name="pet-breed"]').value.trim();
+  const age = document.querySelector('input[name="pet-age"]').value.trim();
+  const color = document.querySelector('input[name="pet-color"]').value.trim();
+  const owner_id = document.querySelector('input[name="pet-owner"]').value.trim();
+
+  console.log(name, type, breed, age, color, owner_id)
+  if (name && type && breed && age && color && owner_id) {
+    const response = await fetch('/api/pets', {
+      method: 'POST',
+      body: JSON.stringify({
+        name,
+        type,
+        breed,
+        age,
+        color,
+        owner_id
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+   
     //   check response
-      if (response.ok) {
-        document.location.reload();
-      } else {
-        alert(response.statusText);
-      }
+    if (response.ok) {
+      document.location.replace('/')
+    } else {
+      alert(response.statusText);
     }
   }
+}
 
 
 
-document.querySelector('.add-pet-form').addEventListener('submit', addPetFormhandler);
+document.querySelector('#add-pet-form').addEventListener('submit', addPetFormhandler);
